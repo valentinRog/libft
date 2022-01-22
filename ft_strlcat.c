00dca7669b02dcbf373bfa226464a173a6d03c76
Valentin Rogister <vrogiste@shi-r9-p5.s19.be>
2022-01-22 04:54:25 +0100

    fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcat.c                                          :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vrogiste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/03 17:18:22 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/01/03 19:03:48 by vrogiste         ###   ########.fr       */
+/*   Created: 2022/01/22 04:45:47 by vrogiste          #+#    #+#             */
+/*   Updated: 2022/01/22 04:49:04 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	src_len = ft_strlen(src);
 	i = dst_len;
 	j = 0;
-	if (dstsize)
-	{
-		while (src[j] && i < dstsize - 1)
-		{
-			dst[i] = src[j];
-			i++;
-			j++;
-		}
-		dst[i] = '\0';
-	}
+	if (dstsize >= dst_len)
+		ft_strlcpy(dst + i, src, dstsize - dst_len);
 	if (dstsize < dst_len)
 		return (src_len + dstsize);
 	else

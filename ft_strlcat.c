7ad331a84a67ce8141eee9ef9a1e898a6d0bfeb1
root <root@DESKTOP-INOLNT2.localdomain>
2022-02-22 19:36:40 +0100

    removed unused variable

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,14 +15,12 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	j;
 	size_t	dst_len;
 	size_t	src_len;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	i = dst_len;
-	j = 0;
 	if (dstsize >= dst_len)
 		ft_strlcpy(dst + i, src, dstsize - dst_len);
 	if (dstsize < dst_len)

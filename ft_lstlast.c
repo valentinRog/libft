e223a3322fce63eba6a8e89f30b75ca220a4e672
valentinRog <valentin.rogister@gmail.com>
2022-07-04 08:52:11 +0200

    yo

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrogiste <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 04:39:14 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/01/04 05:06:23 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/04 08:32:39 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	if (!lst)
-		return (0);
-	if (lst->next)
+	if (lst && lst->next)
 		return (ft_lstlast(lst->next));
 	return (lst);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrogiste <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:21:39 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/01/04 07:49:01 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/04 08:39:02 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = -1;
 	if (!dst && !src)
 		return (NULL);
-	while (++i < n)
-		*((unsigned char *)(dst + i)) = *((unsigned char *)(src + i));
+	if (n)
+	{
+		*(unsigned char *)dst = *(unsigned char *)src;
+		ft_memcpy(dst + 1, src + 1, n - 1);
+	}
 	return (dst);
 }
